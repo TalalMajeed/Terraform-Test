ac1dd6dba62fe449edb490ac3efead5404f4b042
TalalMajeed <m.talal.majeed@gmail.com>
2026-04-20 12:44:20 +0500

    Updated main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,8 @@ resource "azurerm_public_ip" "pip" {
   name                = "vm-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "nic" {
